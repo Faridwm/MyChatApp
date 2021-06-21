7d02f4f6c7538af92ad8c5dd244e071015d09432
cah-kangkung <hafizhun.alim93@gmail.com>
2021-06-21 20:35:09 +0700

    Remove final from ApiClient BASE_URL

--- app/src/main/java/com/fwmubarok/mychatapp/REST/ApiClient.java
+++ app/src/main/java/com/fwmubarok/mychatapp/REST/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "https://fcm.googleapis.com/fcm/";
+    public static String BASE_URL = "https://fcm.googleapis.com/fcm/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
